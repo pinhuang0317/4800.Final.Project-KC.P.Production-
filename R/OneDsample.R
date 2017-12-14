3efc3e2bdf7c2c0a39f2ca038e4a86c31fe61281
pinhuang0317 <pinhuang0317@gmail.com>
2017-12-13 22:32:39 -0600

    Using ggplot instead of hist graph.

--- R/OneDsample.R
+++ R/OneDsample.R
@@ -3,6 +3,9 @@
 #' This function implements single variabel rejection sampling for rvs with bounded support and which have have bounded pdf.
 #'
 #'
+
+ImportS: ggplot2
+
 #' @param f the pdf that we are sampling from
 #' @param N the nimber of attempted samples.
 #' @param lb lower bound of support of f
@@ -11,18 +14,17 @@
 #' @return A vector containing samples from pdf
 #' @export
 #'
-#' @examples
+#' @example
 #'
 #' betaPDF <- function(x) {
 #' ifelse(0 < x & x < 1, 2*x, 0)}
-#' oneDsample(f = betaPDF, N=10000, lb = 0, ub = 1)
+#' a <- oneDsample(f = betaPDF, N=10000, lb = 0, ub = 1)
 #'
-#' hist(oneDsample(f = betaPDF, N=1000000, lb = 0, ub = 1))
+#' ggplot(data.frame(a),aes(a)) + geom_density() + stat_function(fun = betaPDF, color = "red")
 #'
-
 
 oneDsample <- function(f, N, lb, ub) {
-  if (abs(integrate(f, -Inf, Inf)$val - 1) > 0.001) {
+  if (abs(integrate(f, lb, ub)$val - 1) > 0.001) {
     stop("Error: not a pdf. The area under the function you given should be 1")
   }
   else{
@@ -32,5 +34,3 @@ oneDsample <- function(f, N, lb, ub) {
     ones[unis < f(ones)]
   }
 }
-
-
