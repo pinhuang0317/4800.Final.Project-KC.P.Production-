ae1584c7315453d2e1e64daeddad2dad58fa665b
Kitetsu3rd <kunchengjerryfan@gmail.com>
2017-12-13 23:52:39 -0600

    Add the description of Evalue.

--- R/Evalue.R
+++ R/Evalue.R
@@ -1,3 +1,15 @@
+#' what is the function does: Expected Value
+#'
+#' This function is designed to find the expected value for g(x) or g(x, y) given a function g.
+#'
+#' @param g the function that we are looking for expected value
+#' @param rv given random variables
+#'
+#' @return the expected value of the given function
+#' @export
+#'
+#' @examples
+#'
 
 Evalue <-function(g,rv){
   len = length(rv)
