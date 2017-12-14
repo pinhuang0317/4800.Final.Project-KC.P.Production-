3971c0e1ba06bbca550ed8696b0027af0ba67adf
Kitetsu3rd <kunchengjerryfan@gmail.com>
2017-12-14 00:02:37 -0600

    Add two examples.

--- R/Evalue.R
+++ R/Evalue.R
@@ -10,6 +10,19 @@
 #'
 #' @examples
 #'
+#' betaPDF <- function(x) {
+#' ifelse(0 < x & x < 1, 2*x, 0)}
+#' x1 = data.frame(oneDsample(f = betaPDF, N=10000, lb = 0, ub = 1))
+#' g1 = function(x){x^2}
+#' Evalue(g1,x1)
+#'
+#' jointPDF <- function(x){
+#' x1 = x[1]
+#' x2 = x[2]
+#' ifelse(0<x1 & x1 <1 & 0<x2 & x2<1 & 0<x1+x2 & x1+x2<1, 24*x1*x2, 0)}
+#' xy = data.frame(twoDsample(f = jointPDF, N=10000, lbx=0, ubx=1, lby=0, uby=1))
+#' g2 = function(x,y){x^2+y}
+#' Evalue(g2,xy)
 
 Evalue <-function(g,rv){
   len = length(rv)
