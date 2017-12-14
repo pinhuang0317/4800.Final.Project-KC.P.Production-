b1a555177d715d9f6b7654d9144c8f1f0ee35d4e
pinhuang0317 <pinhuang0317@gmail.com>
2017-12-14 00:07:50 -0600

    Add description for Gprob.

--- R/Gprob.R
+++ R/Gprob.R
@@ -1,12 +1,25 @@
+#' what is the function does: Calculate Probability
+#'
+#' This function is designed to calculate the probabilities realted to X or X and Y.
+#'
+#' @param cd the given conditions
+#' @param data the samples obtained from rejection sampling
+#'
+#' @return the value of probability realted to X or X and Y
+#' @export
+#'
+#' @examples
+#'
+#'
 
-Gprob <-function(condition,data){
+Gprob <-function(cd,data){
   len = length(data)
   if (len == 2){
-    meanxy = mean(condition(data$x,data$y))
+    meanxy = mean(cd(data$x,data$y))
     return(meanxy)
   }
   else if (len == 1){
-    meanx = mean(condition(data))
+    meanx = mean(cd(data))
     return(meanx)
   }
 }
