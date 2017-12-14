9981ddab38b9cd6455e1eaf1857a831acc7172c7
pinhuang0317 <pinhuang0317@gmail.com>
2017-12-13 23:41:24 -0600

    Use the uniform distribution to do the rejection sampling for bounded pdf.

--- R/TwoDsample.R
+++ R/TwoDsample.R
@@ -23,10 +23,14 @@ ImportS: MASS
 #' jointPFF <- function(x){
 #' x1 = x[1]
 #' x2 = x[2]
-#' ifelse(0<x1 & x1 <1 & 0<x2 & x2<1 & 0<x1+x2 & x1+x2<1, 24*x1*x2, 0)}
+#' ifelse(0<x1 & x1<1 & 0<x2 & x2<1 & 0<x1+x2 & x1+x2<1, 24*x1*x2, 0)}
 #'
-#' a <- twoDsample(f = jointPFF, N=100, lbx=0, ubx=1, lby=0, uby=1)
+#' f <- function(x){
+#' x1 = x[1]
+#' x2 = x[2]
+#' ifelse(x2>0, 1/pi/(1+x1^2) * 0.05*exp(-0.05*x2), 0)}
 #'
+#' a <- twoDsample(f = f, N=10000)
 #' ggplot(a, aes(x, y)) +  geom_density_2d()
 
 twoDsample <- function(f, N, lbx=-5000, ubx=5000, lby=-5000, uby=5000) {
@@ -34,6 +38,17 @@ twoDsample <- function(f, N, lbx=-5000, ubx=5000, lby=-5000, uby=5000) {
   library(cubature)
   if (abs(adaptIntegrate(f, c(lbx, lby), c(ubx, uby), maxEval=10000)$integral - 1) > 0.001) {
     stop("Error: Bound is missing/wrong or the function is not a pdf. The area under the function you given should be 1")
+  }
+  if (lbx != -5000 & ubx != 5000 & lby != -5000 & uby != 5000){
+    maxf <- max(replicate(100000,f(c(runif(1,lbx,ubx),runif(1,lby,uby)))))
+    twos = c()
+    for (i in 1:N) {
+      two <- c(runif(1,lbx,ubx),runif(1,lby,uby))
+      if (runif(1, 0, maxf) < f(two)){
+        twos = c(twos, two)
+      }
+    }
+    data.frame(x=twos[c(seq(1,length(twos)-1,2))],y=twos[c(seq(2,length(twos),2))])
   }
   else{
     dmvnorm = function(x,mu,sig){
