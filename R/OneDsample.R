b50e6c94bcb7c9d5a1ec99aec8bba94ca32e1f37
pinhuang0317 <pinhuang0317@gmail.com>
2017-12-13 22:22:06 -0600

    Give an example to test our function;
    Hist Graph.

--- R/OneDsample.R
+++ R/OneDsample.R
@@ -7,17 +7,21 @@
 #' @param N the nimber of attempted samples.
 #' @param lb lower bound of support of f
 #' @param ub upper bound of support of f
-#' @param maxf the maximum value of the given function f
 #'
 #' @return A vector containing samples from pdf
 #' @export
 #'
 #' @examples
 #'
+#' betaPDF <- function(x) {
+#' ifelse(0 < x & x < 1, 2*x, 0)}
+#' oneDsample(f = betaPDF, N=10000, lb = 0, ub = 1)
+#' hist(oneDsample(f = betaPDF, N=1000000, lb = 0, ub = 1, maxf = 2))
 #'
 
-oneDsample <- function(f, N, lb, ub, maxf) {
+oneDsample <- function(f, N, lb, ub) {
   ones <- runif(N, lb, ub)
+  maxf <- max(f(runif(100000,lb,ub)))
   unis <- runif(N, 0, maxf)
   ones[unis < f(ones)]
 }
