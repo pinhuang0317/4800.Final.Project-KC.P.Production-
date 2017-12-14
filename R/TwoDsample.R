854fcc8dff1b97f1f83a84e6b595e76da4a57354
Kitetsu3rd <kunchengjerryfan@gmail.com>
2017-12-13 23:31:13 -0600

    Use a more effective method to generate sample of xval and yval.

--- R/TwoDsample.R
+++ R/TwoDsample.R
@@ -7,7 +7,6 @@ ImportS: ggplot2
 ImportS: cubature
 
 #'
-#'
 #' @param fj the joint pdf that we are sampling from
 #' @param N the number of attempted samples.
 #' @param lbx lower bound of support x of f
@@ -34,23 +33,19 @@ twoDsample <- function(fj, N, lbx, ubx, lby, uby) {
     stop("Error: not a pdf. The area under the function you given should be 1")
   }
   else{
-    two <- replicate(N,c(runif(1,lbx,ubx),runif(1,lby,uby)))
-    maxfj <- max(replicate(100000,fj(c(runif(1,lbx,ubx),runif(1,lby,uby)))))
-    unis <- runif(N, 0, maxfj)
-    f1 = -1
-    f3 = 0
+    maxf <- max(replicate(100000,fj(c(runif(1,lbx,ubx),runif(1,lby,uby)))))
     twos = c()
     for (i in 1:N) {
-      f1 = f1 + 2
-      f2 = f1 + 1
-      f3 = f3 + 1
-      if (unis[f3]<fj(c(two[f1],two[f2]))){
-        twos = c(twos, c(two[f1],two[f2]))
+      two <- c(runif(1,lbx,ubx),runif(1,lby,uby))
+      if (runif(1, 0, maxf) < fj(two)){
+        twos = c(twos, two)
       }
     }
     data.frame(x=twos[c(seq(1,length(twos)-1,2))],y=twos[c(seq(2,length(twos),2))])
   }
 }
+
+
 
 
 
