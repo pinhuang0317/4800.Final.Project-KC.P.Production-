5f7526cbd37f85bd19886f8de502e1c1c3a722ec
Kitetsu3rd <kunchengjerryfan@gmail.com>
2017-12-13 23:19:56 -0600

    Add a judging criteria for testing whether this given function f is a pdf or not, by checking whether the intergral area is equal to 1 or not.

--- R/TwoDsample.R
+++ R/TwoDsample.R
@@ -1,18 +1,28 @@
 
 twoDsample <- function(fj, N, lbx, ubx, lby, uby) {
-  two <- replicate(N,c(runif(1,lbx,ubx),runif(1,lby,uby)))
-  maxfj <- max(replicate(100000,fj(c(runif(1,lbx,ubx),runif(1,lby,uby)))))
-  unis <- runif(N, 0, maxfj)
-  f1 = -1
-  f3 = 0
-  twos = c()
-  for (i in 1:N) {
-    f1 = f1 + 2
-    f2 = f1 + 1
-    f3 = f3 + 1
-    if (unis[f3]<fj(c(two[f1],two[f2]))){
-      twos = c(twos, c(two[f1],two[f2]))
-    }
+  library(cubature)
+  if (abs(adaptIntegrate(fj, c(lbx, lby), c(ubx, uby), maxEval=10000)$integral - 1) > 0.001) {
+    stop("Error: not a pdf. The area under the function you given should be 1")
   }
-  data.frame(x=twos[c(seq(1,length(twos)-1,2))],y=twos[c(seq(2,length(twos),2))])
+  else{
+    two <- replicate(N,c(runif(1,lbx,ubx),runif(1,lby,uby)))
+    maxfj <- max(replicate(100000,fj(c(runif(1,lbx,ubx),runif(1,lby,uby)))))
+    unis <- runif(N, 0, maxfj)
+    f1 = -1
+    f3 = 0
+    twos = c()
+    for (i in 1:N) {
+      f1 = f1 + 2
+      f2 = f1 + 1
+      f3 = f3 + 1
+      if (unis[f3]<fj(c(two[f1],two[f2]))){
+        twos = c(twos, c(two[f1],two[f2]))
+      }
+    }
+    data.frame(x=twos[c(seq(1,length(twos)-1,2))],y=twos[c(seq(2,length(twos),2))])
+  }
 }
+
+
+
+
